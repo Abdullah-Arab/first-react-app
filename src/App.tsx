806f23257cba866156ba0b4fdc78dc12c3aa54a5
Abdullah Arab <abdo.arab2014@gmail.com>
2023-10-08 12:42:32 +0200

    loading & bootstrap added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -9,28 +9,46 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-     try{
-      const res = await axios.get<User[]>(
-        "https://jsonplaceholder.typicode.com/xusers"
-      );
-      setUsers(res.data);
-     }
-     catch (e) {
-      setError((e as AxiosError).message);
-    }
-    }
-    fetchUsers();
+    /// async await method ---
+    // const fetchUsers = async () => {
+    //  try{
+    //   const res = await axios.get<User[]>(
+    //     "https://jsonplaceholder.typicode.com/xusers"
+    //   );
+    //   setUsers(res.data);
+    //  }
+    //  catch (e) {
+    //   setError((e as AxiosError).message);
+    // }
+    // }
+    // fetchUsers();
+    const controller = new AbortController();
+    setLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
 
-    // .then((res) => setUsers(res.data))
-    // .catch(err=> setError(err.message));
+    return () => controller.abort();
   }, []);
 
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
